507b0b5fa73de132b84e0ca9abaff97422b49e80
utkarsh-developer <utkarsh.19b101097@abes.ac.in>
2022-01-30 23:16:54 +0530

    initial

---
+++ server.js
@@ -0,0 +1,22 @@
+const expr = require('express')
+const app = expr()
+
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+
+const path = require('path')
+
+const PORT = process.env.PORT || 3300;
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname,  '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
