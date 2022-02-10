12e1f95426210b2c80f555f5cfc7dac2781097a3
utkarsh-developer <utkarsh.19b101097@abes.ac.in>
2022-02-10 11:59:40 +0530

    add cart-page, auth

--- server.js
+++ server.js
@@ -10,13 +10,26 @@ const PORT = process.env.PORT || 3300;
 
 app.use(expr.static('public'))
 
-app.get('/', (req, res)=>{
-    res.render('home')
-})
+
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname,  '/resources/views'))
 app.set('view engine', 'ejs')
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/cart', (req, res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res)=>{
+    res.render('auth/register')
+})
 
 
 app.listen(PORT, ()=>{
