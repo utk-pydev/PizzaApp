68b5f49a769375e6c5224a00c45a9de88308089e
utk-pydev <utkarshvashisth94@gmail.com>
2022-08-21 22:07:50 +0530

    auth system

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const expr = require('express')
 const app = expr()
 
@@ -8,28 +9,72 @@ const path = require('path')
 
 const PORT = process.env.PORT || 3300;
 
+const mongoose = require('mongoose')
+
+const session = require('express-session')
+
+const flash = require('express-flash')
+
+const passport = require('passport')
+
+
+const MongoDbStore = require('connect-mongo')
+
+//Database connection
+
+const url = 'mongodb://localhost:27017/pizza';
+
+
+
+const connection = mongoose.connection;
+
+mongoose.connect(url,
+  err => {
+      if(err) throw err;
+      console.log('Connected to Database')
+  }).catch(
+    err=>{
+      console.log(err);
+      console.log("Failed");
+    }
+  )
+
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60*60*24 },
+    store: MongoDbStore.create({
+        mongoUrl: url
+    }),
+}));
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(expr.json())
 app.use(expr.static('public'))
+app.use(expr.urlencoded({extended:false}));
+///Global middleware
+
+
+app.use((req, res, next)=>{
+  res.locals.session = req.session;
+  res.locals.user = req.user;
+  next()
+})
+
 
 
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname,  '/resources/views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res)=>{
-    res.render('home')
-})
 
-app.get('/cart', (req, res)=>{
-    res.render('customers/cart')
-})
+require('./routes/web')(app)
 
-app.get('/login', (req, res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res)=>{
-    res.render('auth/register')
-})
 
 
 app.listen(PORT, ()=>{
